7ae406997cd239e12ccca4c34b4e04afa8a3e7f7
ishakatwal <katwalishu@gmail.com>
2022-07-02 12:58:37 -0400

    Handled very large number calculations by using bigInt

--- main.go
+++ main.go
@@ -1,28 +1,49 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math/big"
 	"os"
 )
 
 func main() {
 
-	var input int
+	input := new(big.Int)
 	fmt.Print("Enter n'th term to calculate fibonacci for: ")
-	_, err := fmt.Scanln(&input)
+	_, err := fmt.Scanln(input)
 	if err != nil {
+		fmt.Fprintln(os.Stderr, "error processing input: ", err)
 		os.Exit(1)
 	}
-	fmt.Println(fibonacci(input))
+	val, err := fibonacci(input)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error calculating fibonacci: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(val)
+
 }
 
-func fibonacci(input int) (res int) {
-	first, second := 1, 1
+func fibonacci(input *big.Int) (res *big.Int, err error) {
+	first, second := big.NewInt(0), big.NewInt(1)
 
-	for i := 2; i < input; i++ {
-		res = first + second
-		first = second
-		second = res
+	if input.Cmp(big.NewInt(0)) < 0 {
+		return nil, errors.New("invalid input number")
 	}
-	return res
+	// if input is 0, return 0
+	if input.Cmp(big.NewInt(0)) == 0 {
+		return first, nil
+	}
+	// if input is 1 or 2, return 1
+	if input.Cmp(big.NewInt(1)) == 0 || input.Cmp(big.NewInt(2)) == 0 {
+		return second, nil
+	}
+	for i := 2; input.Cmp(big.NewInt(int64(i))) >= 0; i++ {
+		sum := big.NewInt(0)
+		sum.Add(first, second)
+		first = second
+		second = sum
+	}
+	return second, nil
 }
